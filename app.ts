3caa4e74ab7b3b35008636ddebe4efe4b18cd275
rafec <rafael.brizuena@gmail.com>
2023-07-31 18:56:21 -0400

    feat: added deposit method for abstract class DioAccount.ts

--- app.ts
+++ app.ts
@@ -1,9 +1,11 @@
-import { CompanyAccount } from './class/CompanyAccount'
-import { PeopleAccount } from './class/PeopleAccount'
+import { CompanyAccount } from "./class/CompanyAccount";
+import { PeopleAccount } from "./class/PeopleAccount";
 
-const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
-console.log(peopleAccount)
-peopleAccount.deposit()
-const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
-console.log(companyAccount)
+const peopleAccount: PeopleAccount = new PeopleAccount(1, "Nath", 10);
+console.log(peopleAccount);
+peopleAccount.deposit(150);
+peopleAccount.deposit(15);
+const companyAccount: CompanyAccount = new CompanyAccount("DIO", 20);
+companyAccount.deposit(130);
+companyAccount.deposit(15);
+console.log(companyAccount);

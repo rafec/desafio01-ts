bb41e0f1c357c467de6cc9f903ff47bf851b8605
rafec <rafael.brizuena@gmail.com>
2023-07-31 19:35:11 -0400

    fix: deleted setName() method and setted attribute name to readonly

--- class/DioAccount.ts
+++ class/DioAccount.ts
@@ -1,5 +1,5 @@
 export abstract class DioAccount {
-  private name: string;
+  private readonly name: string;
   private readonly accountNumber: number;
   private balance: number = 0;
   private status: boolean = true;
@@ -8,11 +8,6 @@ export abstract class DioAccount {
     this.name = name;
     this.accountNumber = accountNumber;
   }
-
-  setName = (name: string): void => {
-    this.name = name;
-    console.log("Nome alterado com sucesso!!");
-  };
 
   getName = (): string => {
     return this.name;
